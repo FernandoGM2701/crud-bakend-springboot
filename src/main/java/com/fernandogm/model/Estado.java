724a1e504947ca2fae6a0de1cd804ad093dc9612
FernandoGM2701 <fernandogm2701@gmail.com>
2021-12-12 15:10:41 -0500

    Creando las clases modelos para la bd

--- src/main/java/com/fernandogm/model/Estado.java
+++ src/main/java/com/fernandogm/model/Estado.java
@@ -15,4 +15,30 @@ public class Estado {
 	private int id;
 	private String nombre;
 	private Pais pais;
+	
+	public Estado(String nombre, Pais pais) {
+		super();
+		this.nombre = nombre;
+		this.pais = pais;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	
+	
 }
--- src/main/java/com/fernandogm/model/Pais.java
+++ src/main/java/com/fernandogm/model/Pais.java
@@ -14,4 +14,23 @@ public class Pais {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String nombre;
+	
+	public Pais(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
